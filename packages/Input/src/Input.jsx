731925a42823ae8f060b036b61b6a1ba5a36ef9b
Showrin <showrinece15@gmail.com>
2022-02-04 22:27:18 +0600

    Add zerodui/button as dependency of input

--- packages/Input/src/Input.jsx
+++ packages/Input/src/Input.jsx
@@ -1,13 +1,17 @@
+import React from "react";
 import styled from "styled-components";
+import Button from "@zerodui/button";
 
-const Input = styled.input`
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-  font-size: 16px;
-  font-weight: 300;
-  padding: 10px 40px 10px 10px;
-  width: 150px;
-`;
+// {styled.input`
+// border: 1px solid #ccc;
+// border-radius: 4px;
+// box-sizing: border-box;
+// font-size: 16px;
+// font-weight: 300;
+// padding: 10px 40px 10px 10px;
+// width: 150px;
+// `}
+
+const Input = () => <Button>Input</Button>;
 
 export default Input;
